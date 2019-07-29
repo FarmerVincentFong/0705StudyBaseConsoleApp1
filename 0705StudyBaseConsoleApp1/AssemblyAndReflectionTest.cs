17723957b745473ae689cd7c95c48c1efe962147
VincentFang <463890315@qq.com>
2019-07-29 17:25:08 +0800

    将hg的小代码与本地测试代码同步

---
+++ 0705StudyBaseConsoleApp1/AssemblyAndReflectionTest.cs
@@ -0,0 +1,92 @@
+﻿//======================================================================
+// Copyright (C) 2010-2019 广州市宏光软件科技有限公司 
+// 版权所有 
+// 
+// 文件名: AssemblyAndReflectionTest
+// 创建人: 方文谦
+// 创建时间: 2019/7/19 16:57:20
+//======================================================================
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0705StudyBaseConsoleApp1
+{
+    /// <summary>
+    /// 程序集与反射测试
+    /// </summary>
+    public class AssemblyAndReflectionTest
+    {
+        public static void TestRun()
+        {
+            Assembly ass;
+            Type[] types;
+            Type t1;
+            object obj;
+            try
+            {
+                //使用反射，获取动态加载的程序集的某些类型，并进行使用
+                ass = Assembly.LoadFrom(@"F:\FangVincent\visual studio 2015\Projects\ConsoleApplicationTest1\ConsoleApplicationTest1\extlib\TestClassLibrary1.dll");
+                types = ass.GetTypes();
+                foreach (Type tt in types)
+                {
+                    //遍历程序集的类型
+                    ConstructorInfo[] cia = tt.GetConstructors();
+                    Console.WriteLine("构造函数");
+                    ShowArrMsg<ConstructorInfo>(cia);
+
+                    FieldInfo[] fia = tt.GetFields();
+                    Console.WriteLine("字段");
+                    ShowArrMsg(fia);
+
+                    MethodInfo[] mia = tt.GetMethods();
+                    Console.WriteLine("方法");
+                    ShowArrMsg(mia);
+
+                    PropertyInfo[] pia = tt.GetProperties();
+                    Console.WriteLine("属性");
+                    ShowArrMsg(pia);
+
+                    EventInfo[] eia = tt.GetEvents();
+                    Console.WriteLine("事件");
+                    ShowArrMsg(eia);
+                }
+
+                //使用命名空间加类型名获取指定的类型
+                t1 = ass.GetType("TestClassLibrary1.ReflectTestClass");
+                //创建实例
+                obj = ass.CreateInstance("TestClassLibrary1.ReflectTestClass");
+                //获取实例方法
+                MethodInfo m1 = t1.GetMethod("WriteString");
+                Console.WriteLine("动态调用反射方法");
+                Console.WriteLine(m1.Invoke(obj, new object[] { "Tom" }));
+                MethodInfo m2 = t1.GetMethod("WriteName");
+                Console.WriteLine(m2.Invoke(null, new object[] { "Tom" }));
+                MethodInfo m3 = t1.GetMethod("WirteNopara");
+                Console.WriteLine(m3.Invoke(obj, null));
+            }
+            catch (FileNotFoundException fnfe)
+            {
+                Console.WriteLine(fnfe.Message);
+            }
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 遍历泛型枚举序列
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="e"></param>
+        public static void ShowArrMsg<T>(IEnumerable<T> e)
+        {
+            foreach (T item in e)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
---
+++ 0705StudyBaseConsoleApp1/GenericTest1.cs
@@ -0,0 +1,91 @@
+﻿//======================================================================
+// Copyright (C) 2010-2019 广州市宏光软件科技有限公司 
+// 版权所有 
+// 
+// 文件名: GenericTest1
+// 创建人: 方文谦
+// 创建时间: 2019/7/15 9:36:19
+//======================================================================
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationTest1
+{
+    /// <summary>
+    /// 泛型测试
+    /// </summary>
+    public class GenericTest1
+    {
+        public static void TestRun()
+        {
+            object o = null;
+            // Dictionary<,>是一个开放类型，它有2个类型参数
+            Type t1 = typeof(Dictionary<,>);
+            // 创建开放类型的实例（创建失败，出现异常）
+            o = CreateInstance(t1);
+            Console.WriteLine();
+
+            // DictionaryStringKey<>也是一个开放类型，但它有1个类型参数
+            Type t2 = typeof(DictionaryStringKey<>);
+            // 创建该类型的实例（同样会失败，出现异常）
+            o = CreateInstance(t2);
+            Console.WriteLine();
+
+            // DictionaryStringKey<int>是一个封闭类型
+            Type t3 = typeof(DictionaryStringKey<int>);
+            // 创建封闭类型的一个实例（成功）
+            o = CreateInstance(t3);
+            Console.WriteLine($"对象类型={o.GetType()}");
+            //测试泛型类
+            DictionaryStringKey<int> dic = new DictionaryStringKey<int>("werwer");
+            dic.Add("a", 111);
+            dic.Add("b", 222);
+
+            Type t4 = typeof(TestClass1);
+            // 创建抽象类的实例（创建失败，出现异常）
+            o = CreateInstance(t4);
+
+            Console.ReadKey();
+        }
+
+        //声明开放泛型类型
+        public sealed class DictionaryStringKey<T> : Dictionary<string, T>
+        {
+            private string nametest;
+            public DictionaryStringKey()
+            {
+            }
+            public DictionaryStringKey(string name111)
+            {
+                nametest = name111;
+            }
+        }
+
+        //根据元类型创建实例
+        private static object CreateInstance(Type t)
+        {
+            object obj = null;
+            try
+            {
+                // 使用指定类型t的默认构造函数来创建该类型的实例
+                obj = Activator.CreateInstance(t);
+                Console.WriteLine($"已创建{t.ToString()}的实例");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return obj;
+        }
+
+        //使用抽象类测试实例化
+        public abstract class TestClass1
+        {
+            public string name;
+            protected abstract void SetInit(string title);
+        }
+    }
+}
